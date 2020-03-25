e4fc0a8a17a6c6f14831428e93e549243b8bef59
Amel Delic <a.delic@dima-stc.com>
2020-03-25 10:08:56 +0100

    installation script

--- installDocker.sh
+++ installDocker.sh
@@ -4,3 +4,8 @@ sudo apt-get update
 sudo apt-get -y upgrade
 
 sudo apt-get install -y docker docker.io 
+
+
+sudo cp DockerD.service /etc/systemd/system/DockerD.service
+sudo systemctl start DockerD.service
+sudo systemctl enable DockerD.service
